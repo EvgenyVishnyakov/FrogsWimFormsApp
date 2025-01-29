45313dd36c3aa30f7ab38c96b395b605a2ca2550
Evgeny Vishnyakov <vis-evgenij@yandex.ru>
2025-01-29 12:25:06 +0300

    поправил код

--- FrogsWinFormsApp/WelcomeForm.cs
+++ FrogsWinFormsApp/WelcomeForm.cs
@@ -11,16 +11,19 @@ public partial class WelcomeForm : Form
         InitializeComponent();
         StartPosition = FormStartPosition.CenterScreen;
     }
+
     private void StartGameButton_Click(object sender, EventArgs e)
     {
         MainForm mainForm = new MainForm();
         mainForm.ShowDialog();
         this.Close();
     }
+
     private void StartGameButton_MouseHover(object sender, EventArgs e)
     {
         StartGameButton.BackColor = Color.IndianRed;
     }
+
     private void StartGameButton_MouseLeave(object sender, EventArgs e)
     {
         StartGameButton.BackColor = Color.RosyBrown;
